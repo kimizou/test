12d9650b5d557d137c2532a2de11335f7f72805e
kimizou <zqm1987@126.com>
2015-08-22 22:11:54 +0800

    modify

--- src/main/resources/static/js/my.js
+++ src/main/resources/static/js/my.js
@@ -2,8 +2,8 @@
  * Created by admin on 2015/8/19.
  */
 $(function () {
-    $("#myCarousel").carousel({
-        interval: 3000
+    $('.flexslider').flexslider({
+        animation: "slide"
     });
 
     $.goup();
